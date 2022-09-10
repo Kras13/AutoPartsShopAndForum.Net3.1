2fc9b1b94f2e303bb2018bc09fa01e27f9462866
Kras13 <paskalev.1989@abv.bg>
2022-09-10 13:21:04 +0300

    Small changes

--- AutoPartsShopAndForum/AutoPartsShopAndForum.Services.Data/Cart/ProductCartModel.cs
+++ AutoPartsShopAndForum/AutoPartsShopAndForum.Services.Data/Cart/ProductCartModel.cs
@@ -8,6 +8,8 @@
 
         public decimal Price { get; set; }
 
+        public string ImageUrl { get; set; }
+
         public int Quantity { get; set; }
 
         public decimal Total { get; set; }
